183c679b8daa52a5de5fbce38e9e64ddf49134ee
kshitijabits <94720224+kshitijabits@users.noreply.github.com>
2021-11-22 01:40:31 +0530

    Update passcheck.py

--- passcheck.py
+++ passcheck.py
@@ -1,25 +1,34 @@
 import re
 
-char_list1 = ["a-z"]
-char_list2 = ["A-Z"]
-char_list3 = ["0-9"]
 password = input("Enter password:")
 
-print(len(password))
+flag=1
 
 if len(password)<6 or len(password)>16:
     print("\nInvalid length, min length 6 characters max 16.")
 else:
     regex = re.compile('[@_!"#$%^&\'*+,-.()<=@>?/\|}{~:;^_`]') 
     if(regex.search(password) == None):
-        print("\nNo special characters, password invalid.")
-    else: 
-        if (characters in char_list1 for characters in password) and (characters in char_list2 for characters in password) and (characters in char_list3 for characters in password):
-            print("\nValid password created successfully.")
-        else:
-            print("\nInvalid password.")
-#matched_list = [characters in char_list for characters in password]
+        #print("\nNo special characters, password invalid.")
+        flag=0
+        
+    regex1 = re.compile('[a-z]') 
+    if(regex1.search(password) == None):
+        #print("\nNo a-z characters, password invalid.")
+        flag=0
+        
+    regex2 = re.compile('[A-Z]') 
+    if(regex.search(password) == None):
+        #print("\nNo A-Z characters, password invalid.")
+        flag=0
+        
+    regex3 = re.compile('[0-9]') 
+    if(regex3.search(password) == None):
+        #print("\nNo digits 0-9 characters, password invalid.")
+        flag=0
+       
+    if flag==1:
+        print("\nValid password created successfully.")
+    else:
+        print("\nPassword is missing some character from a-z A-Z 0-9 or any special character")
 
-#password_contains_chars = all(matched_list)
-
-#print(password_contains_chars)
